a1dad0fbe53cc475f287c7d040471dc15d7e9043
Henrique Lemos <lemosribeiro@hotmail.com>
2023-10-06 01:10:11 -0300

    Site de Games desenvolvido na UnG

---
+++ site loja games/js/script.js
@@ -0,0 +1,14 @@
+// Exemplo mínimo de JavaScript
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Aguarde o documento ser completamente carregado antes de interagir com os elementos da página
+
+    // Selecione o botão pelo ID (substitua "myButton" pelo ID do seu botão)
+    var button = document.getElementById("myButton");
+
+    // Adicione um evento de clique ao botão
+    button.addEventListener("click", function () {
+        // Ação a ser executada quando o botão é clicado
+        alert("Site em construção!");
+    });
+});
